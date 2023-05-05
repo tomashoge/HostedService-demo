98d93a18bc470c4d526d6a77d65c1c7d12498b0b
Tomas Hogenauer <tomashoge@gmail.com>
2023-05-05 09:52:38 +0200

    Add project files.

---
+++ HostedService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HostedService_demo
+{
+    public class HostedService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
